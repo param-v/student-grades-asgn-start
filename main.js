8da76ea493ca8bb67aae1c6abf86b5a43dbe0174
param-v <112659738+param-v@users.noreply.github.com>
2022-11-08 14:04:17 -0700

    Update main.js

--- main.js
+++ main.js
@@ -51,46 +51,69 @@ function mainMenu() {
 function firstTo40() {
   // Set the grade of the first student to 40.
   outputEl.innerHTML = "First grade to 40";
+  grades[0] = 40;
 }
 
 function lastTo50() {
   // Set the grade of the last student to 50.
   outputEl.innerHTML = "Last grade to 50";
+  grades[grades.length] = 50;
 }
 
 function randomTo100() {
   // Set the grade of a random student to 100.
   outputEl.innerHTML = "Random grade to 100";
+  grades[Math.random() * grades.length] = 100;
 }
 
 function addRandomGrade() {
   // Add a random grade between 0 and 100 to the end of the array.
   outputEl.innerHTML = "Add random grade";
+  grades.push(Math.random() * 100);
 }
 
 function removeLastGrade() {
   // Remove the last grade.
   outputEl.innerHTML = "Remove the last grade";
+  grades.pop();
 }
 
 function countBelow50() {
   // Count how many grades are below 50.  Output the result.
   outputEl.innerHTML = "Count grades below 50";
+  let count = 0;
+  for (let i = 0; i < grades.length; i++) {
+    if (grades[i] < 50) {
+      count++;
+    }
+  }
+  outputEl.innerHTML = count;
 }
 
 function lowGradesTo50() {
   // Change all grades that are below 50 to be equal to 50.
   outputEl.innerHTML = "Change low grades to 50";
+  for (let k = 0; k < grades.length; k++) {
+    if (grades[k] < 50) {
+      grades[k] = 50;
+    }
+  }
 }
 
 function increaseGradesBy10() {
   // Increase each grade by 10%.
   outputEl.innerHTML = "Increase all grades by 10%";
+  for(let i = 0; i < grades.length; i++) {
+    grades[i] += 10;
+  }
 }
 
 function decreaseGradesBy10() {
   // Decrease each grade by 10%.
   outputEl.innerHTML = "Decrease all grades by 10%";
+  for(let i = 0; i < grades.length; i++) {
+    grades[i] -= 10;
+  }
 }
 
 
